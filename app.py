8cfaa23e706517a37be35e878487b0652c82343c
Ashay Changwani <ashaychangwani@gmail.com>
2023-05-11 16:56:09 -0400

    Added memory

--- app.py
+++ app.py
@@ -5,10 +5,15 @@ import streamlit as st
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain, SequentialChain
+from langchain.memory import ConversationBufferMemory
 os.environ['OPENAI_API_KEY'] = apikey
 
 st.title('YoutubeGPT Creator')
 prompt = st.text_input('Plugin the report here')
+
+llm = OpenAI(temperature=0.9)
+
+memory = ConversationBufferMemory(input_key = 'topic', memory_key='chat_history')
 
 title_template = PromptTemplate(
     input_variables = ['topic'],
@@ -19,12 +24,11 @@ script_template = PromptTemplate(
     input_variables = ['title'],
     template='Write me a Youtube Video script based on the title: {title}',
 )
-llm = OpenAI(temperature=0.9)
 
 
 #create title chain
-title_chain = LLMChain(llm = llm, prompt = title_template, verbose = True, output_key='title')
-script_chain = LLMChain(llm = llm, prompt = script_template, verbose = True, output_key='script')
+title_chain = LLMChain(llm = llm, prompt = title_template, verbose = True, output_key='title', memory=memory)
+script_chain = LLMChain(llm = llm, prompt = script_template, verbose = True, output_key='script', memory=memory)
 
 sequential_chain = SequentialChain(chains = [title_chain, script_chain], input_variables=['topic'], output_variables=['title','script'], verbose = True)
 
@@ -33,3 +37,5 @@ if prompt:
     response = sequential_chain({'topic': prompt})
     st.write(response['title'])
     st.write(response['script'])
+    with st.expander('Show Conversation History'):
+        st.info(memory.buffer)
